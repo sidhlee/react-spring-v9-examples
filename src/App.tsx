adf1b17ffd1045c5d50f569474da4a57f3346269
Hayoun Lee <sidhlee@gmail.com>
2020-08-02 19:08:03 +0900

    Finish working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,135 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { loremIpsum } from 'lorem-ipsum';
+import { X } from 'react-feather';
+import {
+  Main,
+  GlobalStyle,
+  Container,
+  Message,
+  Content,
+  Life,
+  Button,
+} from './styles';
+import { useTransition } from 'react-spring';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Learn React Spring</h1>
-    </div>
+const DEBUG = true;
+let id = 0;
+
+type MessageHubProps = {
+  children: (add: (msg: string) => void) => void;
+  timeout?: number;
+  config?: any;
+};
+
+type Message = {
+  key: number;
+  msg: string;
+};
+
+const defaultConfig = {
+  tension: 125,
+  friction: 20,
+  precision: 0.1,
+};
+
+const MessageHub = ({
+  config = defaultConfig,
+  timeout = 3000,
+  children,
+}: MessageHubProps) => {
+  const [refMap] = useState(() => new WeakMap());
+  const [cancelMap] = useState(() => new WeakMap());
+  const [items, setItems] = useState<Message[]>([]);
+
+  const transition = useTransition(items, {
+    key: (item) => item.key,
+    from: { opacity: 0, height: 0, life: '100%' },
+    enter: (item) => async (next, stop) => {
+      if (DEBUG) {
+        console.log(` Entering:`, item.key);
+        console.log(` items: `, items);
+      }
+      // map cancel function to the given item
+      cancelMap.set(item, () => {
+        if (DEBUG) console.log(` Cancelled:`, item.key);
+        stop(); // stop animation for given item
+        setItems((state) => state.filter((i) => i.key !== item.key)); // remove given item
+      });
+      await next({
+        opacity: 1,
+        height: refMap.get(item).offsetHeight,
+        config,
+      });
+      await next({ life: '0%', config: { duration: timeout } });
+      cancelMap.get(item)(); // run cancel function
+    },
+    leave: (item) => async (next) => {
+      if (DEBUG) console.log(` Leaving:`, item.key);
+      await next({ opacity: 0, config });
+      await next({ height: 0, config });
+    },
+  });
+
+  useEffect(
+    () =>
+      void children((msg: string) => {
+        setItems((state) => [...state, { key: id++, msg }]);
+      }), // store passed function in the parent component's ref
+    // so that parent component can have access to the passed function
+    // which has access to all the local variables
+    [children]
   );
-}
+
+  return (
+    <Container>
+      {
+        // props, item
+        transition(({ life, ...style }, item, state) => {
+          if (!items.find((i) => i.key === item.key) && state.phase === 'mount')
+            return;
+          return (
+            <Message style={style}>
+              <Content ref={(elm) => elm && refMap.set(item, elm)}>
+                <Life style={{ right: life }} />
+                <p>{item.msg}</p>
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation(); // to the Container with pointer-events: none;
+                    if (cancelMap.has(item)) {
+                      if (DEBUG) console.log(` Cancelled item:`, item);
+                      cancelMap.get(item)(); // run cancel fn mapped to the item
+                    }
+                  }}
+                >
+                  <X size={18} />
+                </Button>
+              </Content>
+            </Message>
+          );
+        })
+      }
+    </Container>
+  );
+};
+
+const App = () => {
+  const addItemRef = useRef<Function | null>(null);
+
+  const handleMainClick = () => {
+    if (DEBUG) console.log('[Main] clicked');
+    if (typeof addItemRef.current === 'function') {
+      addItemRef.current(loremIpsum());
+    }
+  };
+
+  return (
+    <Main className="main" onClick={handleMainClick}>
+      <GlobalStyle />
+      Click here to create notification
+      {/* invoke children fn passing in addItem fn to be stored in ref */}
+      <MessageHub children={(addItem) => (addItemRef.current = addItem)} />
+    </Main>
+  );
+};
 
 export default App;
---
+++ src/styles.tsx
@@ -0,0 +1,104 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import { animated } from 'react-spring';
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    user-select: none;
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: #f0f0f0;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Main = styled('div')`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #676767;
+`;
+
+type ContainerProps = {
+  top?: boolean;
+  position?: 'center' | 'start' | 'end';
+};
+
+export const Container = styled('div')<ContainerProps>`
+  position: fixed;
+  z-index: 1000;
+  margin: 0 auto;
+  width: auto;
+  top: ${(props) => (props.top ? '30px' : 'unset')};
+  bottom: ${(props) => (props.top ? 'unset' : '30px')};
+  left: 30px;
+  right: 30px;
+  display: flex;
+  flex-direction: ${(props) => (props.top ? 'column-reverse' : 'column')};
+  pointer-events: none;
+  align-items: ${(props) =>
+    props.position === 'center' ? 'center' : `flex-${props.position || 'end'}`};
+  @media (max-width: 860px) {
+    align-items: center;
+  }
+`;
+
+export const Message = styled(animated.div)`
+  position: relative;
+  width: 40ch;
+  @media (max-width: 680px) {
+    width: 100%;
+  }
+`;
+
+export const Content = styled('div')<{ canClose?: boolean }>`
+  cursor: auto;
+  position: relative;
+  color: white;
+  background: #445159;
+  opacity: 0.9;
+  padding: 12px 22px;
+  font-size: 1em;
+  display: grid;
+  grid-template-columns: ${(props) =>
+    props.canClose === false ? '1fr' : '1fr auto'};
+  grid-gap: 10px;
+`;
+
+export const Button = styled('button')`
+  cursor: pointer;
+  pointer-events: all;
+  outline: none;
+  border: none;
+  align-self: flex-end;
+  margin: 0;
+  padding: 0;
+  background: transparent;
+  padding-bottom: 14px;
+  color: rgba(255, 255, 255, 0.5);
+  :hover {
+    color: rgba(255, 255, 255, 0.6);
+  }
+`;
+
+export const Life = styled(animated.div)<{ top?: boolean }>`
+  position: absolute;
+  background-image: linear-gradient(130deg, #00b4e6, #00f0e0);
+  height: 5px;
+  left: 0px;
+  right: 25%;
+  bottom: ${(props) => (props.top ? '10px' : '0')};
+`;
