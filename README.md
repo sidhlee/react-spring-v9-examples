2b5e3800d364eae49622e31461cd08ff6526e1aa
Hayoun Lee <sidhlee@gmail.com>
2020-08-13 19:09:12 +0900

    Add fade animation when navigating

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # React Spring Examples (v9)
+
+## Transitioning between components
+
+Mounting component will enter as soon as the previous component starts leaving. This causes entering component to be rendered next to the leaving component and suddenly change its position when the previous component unmounts.
+
+You can add absolute-positioning to the animated container to avoid this:
+
+```tsx
+const location = useLocation();
+const transition = useTransition(location, {
+  from: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+  },
+  leave: {
+    opacity: 0,
+  },
+});
+return (
+  <>
+    <GlobalStyle />
+    <HomeLink to="/">ReactSpring</HomeLink>
+    {transition((style, location) => (
+      <Suspense fallback={<div>Loading...</div>}>
+        <animated.div
+          style={{
+            ...style,
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Switch location={location}>
+            <Route path="/" exact>
+              <Navigation />
+            </Route>
+            <Route path="/message-hub" component={MessageHub} />
+            <Route path="/trees" component={Trees} />
+            <Route path="/dropdown-menu" component={Navbar} />
+            <Route path="/carousel" component={Carousel} />
+          </Switch>
+        </animated.div>
+      </Suspense>
+    ))}
+  </>
+);
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 import Navigation from './Navigation';
 import { GlobalStyle, HomeLink } from './styles';
@@ -10,22 +11,44 @@ const Navbar = lazy(() => import('./examples/dropdownMenu/Navbar'));
 const Carousel = lazy(() => import('./examples/carousel/Carousel'));
 
 const App = () => {
+  const location = useLocation();
+  const transition = useTransition(location, {
+    from: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+    },
+    leave: {
+      opacity: 0,
+    },
+  });
   return (
     <>
       <GlobalStyle />
       <HomeLink to="/">ReactSpring</HomeLink>
-      {/* invoke children fn passing in addItem fn to be stored in ref */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path="/" exact>
-            <Navigation />
-          </Route>
-          <Route path="/message-hub" component={MessageHub} />
-          <Route path="/trees" component={Trees} />
-          <Route path="/dropdown-menu" component={Navbar} />
-          <Route path="/carousel" component={Carousel} />
-        </Switch>
-      </Suspense>
+      {transition((style, location) => (
+        <Suspense fallback={<div>Loading...</div>}>
+          <animated.div
+            style={{
+              ...style,
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <Switch location={location}>
+              <Route path="/" exact>
+                <Navigation />
+              </Route>
+              <Route path="/message-hub" component={MessageHub} />
+              <Route path="/trees" component={Trees} />
+              <Route path="/dropdown-menu" component={Navbar} />
+              <Route path="/carousel" component={Carousel} />
+            </Switch>
+          </animated.div>
+        </Suspense>
+      ))}
     </>
   );
 };
