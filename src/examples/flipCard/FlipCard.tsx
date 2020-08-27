c2783bb4bdfc6be266d3c5e04d5536f71433c057
Hayoun Lee <sidhlee@gmail.com>
2020-08-27 15:49:59 +0900

    Finish FlipCard

--- src/examples/flipCard/FlipCard.tsx
+++ src/examples/flipCard/FlipCard.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring } from 'react-spring';
 
-import { FlipCardContainer } from './flip-card-styles';
+import { FlipCardContainer, CardFront, CardBack } from './flip-card-styles';
 
 type FlipCardProps = {};
 
 const FlipCard = (props: FlipCardProps) => {
-  return <FlipCardContainer>FlipCard</FlipCardContainer>;
+  const [flipped, set] = useState(false);
+  const { opacity, transform } = useSpring({
+    opacity: flipped ? 0 : 1,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: {
+      mass: 5,
+      tension: 500,
+      friction: 80,
+    },
+  });
+
+  return (
+    <FlipCardContainer onClick={() => set((s) => !s)}>
+      <CardFront style={{ opacity, transform }} />
+      <CardBack
+        style={{
+          opacity: opacity.to((o) => 1 - o),
+          transform: transform.to((t) => `${t} rotateX(180deg)`),
+        }}
+      />
+    </FlipCardContainer>
+  );
 };
 
 export default FlipCard;
--- src/examples/flipCard/flip-card-styles.tsx
+++ src/examples/flipCard/flip-card-styles.tsx
@@ -1,8 +1,32 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { a } from 'react-spring';
 
 export const FlipCardContainer = styled('div')`
   width: 100%;
   min-height: 100vh;
-  display: grid;
-  place-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const cardCss = css`
+  position: absolute;
+  max-width: 400px;
+  max-height: 400px;
+  width: 90%;
+  height: 50ch;
+  cursor: pointer;
+  will-change: transform, opacity;
+  background-size: cover;
+  border-radius: var(--border-radius);
+`;
+
+export const CardFront = styled(a.div)`
+  ${cardCss}
+  background-image: url(https://picsum.photos/id/130/500/500);
+`;
+
+export const CardBack = styled(a.div)`
+  ${cardCss}
+  background-image: url(https://picsum.photos/id/112/500/500);
 `;
